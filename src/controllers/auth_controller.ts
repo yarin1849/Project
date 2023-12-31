849ee9ff1de40c9aa3248b38fc80b7c3e406bcf3
Yarin <yarin1849@gmail.com>
2023-12-31 13:27:40 +0200

    adding implementation of refresh token

--- src/controllers/auth_controller.ts
+++ src/controllers/auth_controller.ts
@@ -17,7 +17,7 @@ const register = async (req: Request, res: Response) => {
         const salt = await bcrypt.genSalt(10);
         const encryptedPassword = await bcrypt.hash(password, salt);
         const rs2 = await User.create({ 'email': email, 'password': encryptedPassword });
-        return res.status(201).send({ '_id': rs2._id });
+        return res.status(201).send(rs2);
     } catch (err) {
         return res.status(400).send("error missing email or password");
     }
@@ -39,21 +39,81 @@ const login = async (req: Request, res: Response) => {
             return res.status(401).send("email or password incorrect");
         }
 
-        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRATION });
-        return res.status(200).send({ 'accessToken': token });
+        const accessToken = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRATION });
+        const refreshToken = jwt.sign({ _id: user._id }, process.env.JWT_REFRESH_SECRET);
+        if (user.refreshTokens == null) {
+            user.refreshTokens = [refreshToken];
+        } else {
+            user.refreshTokens.push(refreshToken);
+        }
+        await user.save();
+        return res.status(200).send({
+            'accessToken': accessToken,
+            'refreshToken': refreshToken
+        });
     } catch (err) {
         return res.status(400).send("error missing email or password");
     }
 }
 
-
-
 const logout = async (req: Request, res: Response) => {
-    res.status(400).send("unimplemented");
+    const authHeader = req.headers['authorization'];
+    const refreshToken = authHeader && authHeader.split(' ')[1]; // Bearer <token>
+    if (refreshToken == null) return res.sendStatus(401);
+    jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, async (err, user: { '_id': string }) => {
+        console.log(err);
+        if (err) return res.sendStatus(401);
+        try {
+            const userDb = await User.findOne({ '_id': user._id });
+            if (!userDb.refreshTokens || !userDb.refreshTokens.includes(refreshToken)) {
+                userDb.refreshTokens = [];
+                await userDb.save();
+                return res.sendStatus(401);
+            } else {
+                userDb.refreshTokens = userDb.refreshTokens.filter(t => t !== refreshToken);
+                await userDb.save();
+                return res.sendStatus(200);
+            }
+        } catch (err) {
+            res.sendStatus(401).send(err.message);
+        }
+    });
+}
+
+const refresh = async (req: Request, res: Response) => {
+    const authHeader = req.headers['authorization'];
+    const refreshToken = authHeader && authHeader.split(' ')[1]; // Bearer <token>
+    if (refreshToken == null) return res.sendStatus(401);
+    jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, async (err, user: { '_id': string }) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(401);
+        }
+        try {
+            const userDb = await User.findOne({ '_id': user._id });
+            if (!userDb.refreshTokens || !userDb.refreshTokens.includes(refreshToken)) {
+                userDb.refreshTokens = [];
+                await userDb.save();
+                return res.sendStatus(401);
+            }
+            const accessToken = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRATION });
+            const newRefreshToken = jwt.sign({ _id: user._id }, process.env.JWT_REFRESH_SECRET);
+            userDb.refreshTokens = userDb.refreshTokens.filter(t => t !== refreshToken);
+            userDb.refreshTokens.push(newRefreshToken);
+            await userDb.save();
+            return res.status(200).send({
+                'accessToken': accessToken,
+                'refreshToken': refreshToken
+            });
+        } catch (err) {
+            res.sendStatus(401).send(err.message);
+        }
+    });
 }
 
 export default {
     register,
     login,
-    logout
+    logout,
+    refresh
 }
